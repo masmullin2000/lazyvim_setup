a0145c25cd78e56654b0dbae5a62adddae999b0d
Michael Mullin <masmullin@gmail.com>
2024-05-23 23:20:24 -0400

    may 2024 updates

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -10,6 +10,13 @@ end
 
 return {
     {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
+    },
+    {
         "hrsh7th/nvim-cmp",
         config = function()
             local cmp = require("cmp")
